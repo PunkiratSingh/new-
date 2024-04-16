2222d164379cb48e3e9b4a1f2d46db7b592abac3
PunkiratSingh <145853550+PunkiratSingh@users.noreply.github.com>
2024-04-16 17:03:00 +0530

    Add files via upload

---
+++ powershell.ps1
@@ -0,0 +1,3 @@
+$sourceFilePath ="C:\Users\punki\OneDrive\Desktop\New folder\abc.txt"
+$destinationFilePath ="C:\Users\punki\OneDrive\Desktop"
+Copy-Item -Path $sourceFilePath -Destination $destinationFilePath
